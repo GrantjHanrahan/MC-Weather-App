522acc179dd87b6806480cf49a64212815eb6fce
Grant Hanrahan <grantjhanrahan@gmail.com>
2018-03-31 17:05:29 +1100

    added 5 day forecast

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/app'));
 
 // Test default route
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname+'/index.html'));
+  res.sendFile(path.join(__dirname+'/app/index.html'));
 });
 
 // Start server
